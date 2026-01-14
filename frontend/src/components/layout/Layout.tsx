631c92388293af8c368cf324f38de8914a82b8e3
Simon Sjöö <simon.sjoo@forefront.se>
2026-01-14 12:52:53 +0100

    Standard layout file

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/material'
+import { Outlet } from 'react-router-dom'
+
+export function Layout() {
+  return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        bgcolor: 'background.default',
+      }}
+    >
+      <Box component="main" sx={{ flexGrow: 1, py: 3 }}>
+        <Outlet />
+      </Box>
+    </Box>
+  )
+}
